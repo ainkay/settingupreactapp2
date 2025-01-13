014ce28f03a656cd668f98a1f39ae1a96d21a934
ainkay <ain.kay@kalvium.community>
2025-01-13 11:42:42 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My React App</h1>
+      <p><strong>Name:</strong> Ain Kayy</p>
+      <p><strong>Contact:</strong> ain.kay@kalvium.community</p>
+    </div>
+  );
+};
+
+export default Home;
